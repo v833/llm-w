9c9ef667935ecaba5e2608afa239aa2c0aa17614
v833 <2507301541@qq.com>
2026-02-08 18:26:41 +0800

    feat: 添加天气工具并改用流式输出
    - 新增 get_weather 函数作为天气查询工具
    - 将 agent.invoke 改为 agent.stream 以实现流式响应
    - 更新用户查询内容以测试天气工具功能

--- agent-1.py
+++ agent-1.py
@@ -1,26 +1,45 @@
 from langchain.agents import create_agent
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
+
+def get_weather(city: str) -> str:
+    """获取城市的天气"""
+    return f"{city}的天气是晴朗的"
+
+
 agent = create_agent(
     model="deepseek-chat",
+    tools=[get_weather],
 )
 
-results = agent.invoke(
+# for event in agent.stream(
+#     {
+#         "messages": [
+#             {
+#                 "role": "user",
+#                 "content": "你好, 上海的天气怎么样?",
+#             }
+#         ],
+#     },
+#     stream_mode="values",
+# ):
+
+#     messages = event["messages"]
+
+#     print(messages[-1].pretty_print())
+
+
+for chunk in agent.stream(
     {
         "messages": [
             {
                 "role": "user",
-                "content": "你好",
+                "content": "你好, 上海的天气怎么样?",
             }
-        ]
-    }
-)
-
-messages = results["messages"]
-
-
-for message in messages:
-    message.pretty_print()
+        ],
+    },
+    stream_mode="messages",
+):
+    print(chunk[0].content, flush=True, end="")
